80a9b4c5841849f738997a100fcdfc037d6163bc
Jordan Day <jordaeday22@gmail.com>
2026-04-14 16:04:18 -0700

    added basic functionality (no styling)

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,35 @@ struct CurrentWeather {
     weather_code: i32,
 }
 
+#[derive(Debug, Deserialize)]
+struct GeocodingResponse {
+    results: Vec<GeocodingResult>,
+}
+
+#[derive(Debug, Deserialize)]
+struct GeocodingResult {
+    latitude: f64,
+    longitude: f64,
+    #[serde(rename = "admin1")]
+    state: String,
+}
+
+fn capitalize(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args: Vec<String> = std::env::args().skip(1).collect();
-    if args.len() < 2 {
-        eprintln!("Usage: weather <city-name> <state-name>");
+    if args.len() < 1 {
+        eprintln!("Usage: weather <city-name>");
         std::process::exit(1);
     }
-    let city_name = args[0].clone();
-    let state_name = args[1].clone();
+    let city_name = capitalize(&args[0]);
 
     #[cfg(feature = "framebuffer")] {
         use slint_backend_linuxfb::LinuxFbPlatformBuilder;
@@ -37,15 +57,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         slint::platform::set_platform(Box::new(platform)).unwrap();
     }
 
-    // For getting weather, first get gecoords, then weather
+    // For getting weather, first get gecoords, then use those for weather
     // geocoding api
-    // hardcode for now, update later
-    let LATITUDE: f64 = 47.60621;
-    let LONGITUDE: f64 = -122.33207;
+    let geocoding_url = format!("https://geocoding-api.open-meteo.com/v1/search?name={}", city_name);
+    let geocoding_response = reqwest::blocking::get(&geocoding_url)?.json::<GeocodingResponse>()?;
+    let latitude: f64 = geocoding_response.results[0].latitude;
+    let longitude: f64 = geocoding_response.results[0].longitude;
+    let state_name = &geocoding_response.results[0].state;
+
+    println!("Coordinates for {}, {}: {}, {}", city_name, state_name, latitude, longitude);
 
     // weather api
-    let url = format!("https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m,weather_code&wind_speed_unit=mph&temperature_unit=fahrenheit&timezone=auto", LATITUDE, LONGITUDE);
-    let response = reqwest::blocking::get(&url)?.json::<WeatherResponse>()?;
+    let weather_url = format!("https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m,weather_code&wind_speed_unit=mph&temperature_unit=fahrenheit&timezone=auto", latitude, longitude);
+    let response = reqwest::blocking::get(&weather_url)?.json::<WeatherResponse>()?;
     println!("Current temperature in {}, {}: {}°F, weather code: {}", city_name, state_name, response.current.temperature, response.current.weather_code);
 
     let main_window = MainWindow::new()?;
@@ -53,7 +77,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     main_window.set_temperature(response.current.temperature as f32);
     main_window.set_weather_code(response.current.weather_code);
     main_window.set_city_name(city_name.into());
-
+    main_window.set_state_name(state_name.clone().into());
     main_window.run()?;
     Ok(())
 }
